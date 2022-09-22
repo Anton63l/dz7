e857e584cfa57eb680f72032d74f1d455b42863e
Anton <anton@mail.ru>
2022-09-22 23:43:45 +0400

    Решена третья задача

--- Program.cs
+++ Program.cs
@@ -31,41 +31,77 @@
 
 
 // Заполнение рандомного массива и вывод на печать
-void FillAndPrintArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-            Console.Write(array[i, j] + " ");
+// void FillAndPrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//             Console.Write(array[i, j] + " ");
 
-        }
-        Console.WriteLine();
+//         }
+//         Console.WriteLine();
 
-    }
-}
+//     }
+// }
 
-void ReturnElement(int[,]array, int rows, int cols){
-    Console.WriteLine();
+// void ReturnElement(int[,]array, int rows, int cols){
+//     Console.WriteLine();
 
-    if (rows < array.GetLength(0) && cols < array.GetLength(1)) Console.WriteLine(array[rows, cols]);
-    else Console.WriteLine($"{rows}{cols} -> такого числа в массиве нет");
+//     if (rows < array.GetLength(0) && cols < array.GetLength(1)) Console.WriteLine(array[rows, cols]);
+//     else Console.WriteLine($"{rows}{cols} -> такого числа в массиве нет");
 
-}
-
-
-Console.Write("Введите номер строки элемента с нумерацией от 0: ");
-int x = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите номер строки элемента с нумерацией от 0: ");
-int y = Convert.ToInt32(Console.ReadLine());
-
-int[,] matrix = new int[new Random().Next(1, 10), new Random().Next(1, 10)];
-
-FillAndPrintArray(matrix);
-ReturnElement(matrix,x,y);
+// }
 
 
+// Console.Write("Введите номер строки элемента с нумерацией от 0: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите номер строки элемента с нумерацией от 0: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// int[,] matrix = new int[new Random().Next(1, 10), new Random().Next(1, 10)];
+
+// FillAndPrintArray(matrix);
+// ReturnElement(matrix,x,y);
 
 
+
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// 
+
+// Задаем массив рандомных чисел и выводим его на печать
+// int[,] Matrix(int rows, int cols)
+// {
+//     int[,] matrix = new int[rows, cols];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+
+//             matrix[i, j] = new Random().Next(0, 10);
+//             Console.Write($"{matrix[i, j]}\t ");
+//         }
+//         Console.WriteLine();
+//     }
+//    Console.WriteLine();
+//     return matrix;
+// }
+
+
+// // Подсчитываем среднее арифметическое
+// void AverageSum(int[,] arr)
+// {
+// for (int j = 0; j < arr.GetLength(1); j++)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         sum += arr[i, j];
+//     }
+//     Console.Write($"Среднее арифметическое столбца {j + 1} --> { sum / arr.GetLength(0)}\t");
+// }
+// }
+
+// AverageSum(Matrix(3,4));
